2885818e2ec8df7d578a9a39d5a22bee4c557210
hrobles <hector.manuel.robles@autozone.com>
2024-06-26 12:21:21 -0600

    Effect hook base

--- src/components/BurritosForDelivery.js
+++ src/components/BurritosForDelivery.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { DeliveryService } from "../services/DeliveryService";
 
 const BurritosForDelivery = () => {
+  const [delivery, setDelivery] = useState({
+    name: "",
+    restaurant: 1,
+    hour: "",
+    close: false,
+    minutesToClose: "",
+  });
+
   const submitForm = () => {
-    console.info("Oh you submitted the form!");
+    DeliveryService.saveDeliveryList([delivery]);
+  };
+
+  const updateDeliveryData = (prop, data) => {
+    let newData = { ...delivery };
+    newData[prop] = data;
+    setDelivery(newData);
   };
 
   return (
@@ -10,29 +25,61 @@ const BurritosForDelivery = () => {
       <div>
         <label htmlFor="name">What's your name:</label>
         <br />
-        <input type="text" id="name" name="fname" />
+        <input
+          type="text"
+          id="name"
+          name="fname"
+          value={delivery.name}
+          onChange={(e) => updateDeliveryData("name", e.target.value)}
+        />
         <br />
         <label htmlFor="restaurant">Where are you picking burritos from:</label>
         <br />
-        <select id="restaurant" name="frestaurant">
-          <option value=""></option>
+        <select
+          id="restaurant"
+          name="frestaurant"
+          defaultValue={delivery.restaurant}
+          onChange={(e) => updateDeliveryData("restaurant", e.target.value)}
+        >
+          <option value="0">None</option>
+          <option value="1">Default</option>
         </select>
         <br />
         <label htmlFor="hour">Enter arrival time to the place:</label>
         <br />
-        <input type="time" id="hour" name="fhour" />
+        <input
+          type="time"
+          id="hour"
+          name="fhour"
+          value={delivery.hour}
+          onChange={(e) => updateDeliveryData("hour", e.target.value)}
+        />
         <br />
         <label htmlFor="close">Automatically close requests?</label>
         <br />
-        <input type="checkbox" id="close" name="fclose" />
+        <input
+          type="checkbox"
+          id="close"
+          name="fclose"
+          checked={delivery.close}
+          onChange={() => updateDeliveryData("close", !delivery.close)}
+        />
         <br />
         <label htmlFor="timeToClose">
-          Close request on{" "}
-          <input type="number" id="timeToClose" name="ftimeToClose" /> minutes
-          before arrival time
+          Close request on
+          <input
+            type="number"
+            id="timeToClose"
+            name="ftimeToClose"
+            value={delivery.minutesToClose}
+            onChange={(e) =>
+              updateDeliveryData("minutesToClose", e.target.value)
+            }
+          />
+          minutes before arrival time
         </label>
         <br />
-        <button onClick={submitForm}>Submit</button>
+        <button onClick={() => submitForm()}>Submit</button>
       </div>
     </div>
   );
--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -3,12 +3,12 @@ import utils from "./utils";
 const DataUtils = {
   async readData(dataName) {
     await utils.delay(1000);
-    return window.localStorage.getItem(dataName) || [];
+    return JSON.parse(window.localStorage.getItem(dataName)) || [];
   },
 
   async writeData(dataName, data) {
     await utils.delay(1000);
-    window.localStorage.setItem(dataName, data);
+    window.localStorage.setItem(dataName, JSON.stringify(data));
   },
 };
 
